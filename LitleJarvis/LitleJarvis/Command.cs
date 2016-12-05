36fe560862778fafe4b3ec0ed104dc0d9e29e16a
Anton Bielov <us5nar@gmail.com>
2016-12-05 19:01:36 +0200

    Little Jarvis :) first commit

---
+++ LitleJarvis/LitleJarvis/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LitleJarvis
+{
+    abstract class Command:ICommand
+    {
+        public string Caption;
+        public string Usage;
+        abstract public void Action(string UserInput);
+        abstract public bool CheckInCommand(string UserInput);
+    }
+}
